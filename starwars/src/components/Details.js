ef194e38b83b7ebebeaec552abfe2f821d04592f
BrettMcAdams96@gmail.com <brettmcadams96@gmail.com>
2020-08-14 13:31:56 -0400

    finished some strech goals also

--- starwars/src/components/Details.js
+++ starwars/src/components/Details.js
@@ -1,7 +1,22 @@
 import React from 'react'
-import styled from 'styled-components'
+import styled, { keyframes } from 'styled-components'
+
+const kf = keyframes`
+  100% {
+    opacity: 1;
+  }
+`
 
 const StyledDetails = styled.div`
+
+    opacity: 0;
+    animation: ${kf} 0.3s ease-in-out forwards;
+    border: 1px solid #d2d2d2;
+    box-shadow: 0px 1px 6px -2px white;
+    border-radius: 8px;
+    margin-top: 16px;
+    padding: 16px;
+
   button {
     background: transparent;
     border-radius: 3px;
@@ -11,6 +26,7 @@ const StyledDetails = styled.div`
     padding: 0.25em 1em; 
   }
 `
+
 
 export default function Details(props) {
     const { info, id, close } = props  
